f72cbbb484545179e101240ea1582d3412e70a8d
devatquarxps <devatquarxps@gmail.com>
2017-09-09 15:22:56 +0530

    project setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <header>
+
+    </header>
+    <main>
+
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
